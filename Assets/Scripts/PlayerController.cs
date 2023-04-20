23ad503054ce8d0687ac2dca6ee55f64ffeb5395
Artem Anikin <nicebear35@gmail.com>
2023-04-20 15:17:43 +0300

    Deleting unused "usings"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -52,7 +50,6 @@ public class PlayerController : MonoBehaviour
         Damaged?.Invoke();
     }
 
-    [System.Obsolete]
     public void Heal()
     {
         var particleColor = _particleSystem.colorOverLifetime;
